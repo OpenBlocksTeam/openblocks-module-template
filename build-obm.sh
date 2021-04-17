f10df49022f214178989cc10cb1832db1add1b53
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-17 18:36:58 +0700

    feat: Check if there are multiple jars

--- build-obm.sh
+++ build-obm.sh
@@ -15,6 +15,36 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
+# Check if there are multiple jars, and if it is, make the user choose which jar they want to include
+
+jar=
+
+# Get every jars in build/libs/
+jars=(build/libs/*.jar)
+
+# Check if there are more than 1 jar in jars
+if [ ${#jars[@]} -gt 1 ]; then
+  echo "There are more than one jar in the build directory, choose one"
+
+  count=0
+
+  for ljar in $jars
+  do
+    echo "[$count] $ljar"
+    count=$(($count + 1))
+  done
+
+  selected=0
+  read selected
+
+  jar=${jars[selected]}
+
+  echo "Selected jar: ${jars[selected]}"
+else
+  # There is just one jar, use that
+  jar=${jars[0]}
+fi
+
 echo "Checking if the necessary commands exists ======"
 
 # Check if wget, java, and zip exists
@@ -44,21 +74,23 @@ fi
 
 echo "Dex-ing the jar ================================"
 
+out_filename=$(basename -- $jar)
+
 # Dex the jar
-java -jar d8.jar --release --classpath libs/openblocks-module-interface.jar libs/android-30.jar --min-api 21 --output out.jar build/libs/*.jar
+java -jar d8.jar --release --min-api 21 --output $out_filename libs/openblocks-module-interface.jar libs/android-30.jar $jar
 
 echo "Zipping the files =============================="
 
 # Zip em to release.zip
-zip release.zip openblocks-module-manifest.json out.jar
+zip release.zip openblocks-module-manifest.json $out_filename
 
 # then rename it to obm, OpenBlocks Module, where it can be loaded into the app
 mv release.zip release.obm
 
-echo "Removing out.jar ==============================="
+echo "Removing unused jar ============================"
 
-# Delete out.jar
-rm out.jar
+# Delete the dex-ed jar
+rm $out_filename
 
 echo "===================== Done ====================="
 echo "Saved as release.obm"
